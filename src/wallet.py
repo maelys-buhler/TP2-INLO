a0fed04d0e9cb810e62a0fa182fbe009dfc6aefe
Bühler Maëlys <maelys.buhler@he-arc.ch>
2024-03-19 11:26:59 +0100

    remove code smells

--- src/wallet.py
+++ src/wallet.py
@@ -13,18 +13,11 @@ class Wallet(object):
     def __init__(self, initial_amount=0):
         self.balance = initial_amount
 
-    def spend_cash(self, amount, deferred=False):
-        """Removes the specified amount of money from the current Wallet object"""
-        if self.balance < amount:
-            raise InsufficientAmount(f"Not enough available to spend {amount}")
-
-        self.balance -= amount
-
     def add_cash(self, amount):
         """Adds the specified amount of money to the current Wallet object"""
         self.balance += amount
 
-    def spend_money(self, amount, deferred=False):
+    def spend_money(self, amount):
         """Removes the specified amount of money from the current Wallet object"""
         if self.balance < amount:
             raise InsufficientAmount(f"Not enough available to spend {amount}")
